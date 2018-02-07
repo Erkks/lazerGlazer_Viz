c7d698ee99d22907867e28b795b05ca8c7527413
Glitch (fields-demo) <none>
2018-02-07 00:47:31 +0000

    factored out colors

--- README.md
+++
@@ -1,5 +0,0 @@
-# lazerGlazer_Viz
-Fields demo
-
-The demo is hosted on glitch for viewing: https://fields-demo.glitch.me/  
-You can edit the demo code here: https://glitch.com/edit/#!/fields-demo
--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 let sky = document.querySelector('a-sky');
 let pillar = document.querySelector('a-cylinder')
+
 let animationDuration = 15000
+
+let color1 = getRandomColor() //"66d9f9"
+let color2 = getRandomColor() //"FFFD9B"
+let color3 = getRandomColor() //"b62bd8"
+let color4 = getRandomColor() //"19ffe0"
 
 // random num generator
 function getRandomNumber(x, y) {
@@ -20,12 +26,12 @@ function getRandomColor() {
 
 
 // set pillar values
-pillar.setAttribute('color', `#${getRandomColor()}`);
+pillar.setAttribute('color', `#${color1}`);
 pillar.setAttribute('open-ended', `true`);
 pillar.setAttribute('radius', `1.5`);
 pillar.setAttribute('height', `10000`);
-pillar.setAttribute('animation__color', `property: color; dir: alternate; dur: ${animationDuration}; easing: easeInOutSine; loop: true; to: #${getRandomColor()}`);
+pillar.setAttribute('animation__color', `property: color; dir: alternate; dur: ${animationDuration}; easing: easeInOutSine; loop: true; to: #${color2}`);
 
 // set sky values
-sky.setAttribute('color', `#${getRandomColor()}`);
-sky.setAttribute('animation__color', `property: color; dir: alternate; dur: ${animationDuration}; easing: easeInOutSine; loop: true; to: #${getRandomColor()}`);
+sky.setAttribute('color', `#${color3}`);
+sky.setAttribute('animation__color', `property: color; dir: alternate; dur: ${animationDuration}; easing: easeInOutSine; loop: true; to: #${color4}`);
